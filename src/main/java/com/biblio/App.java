65e00af2b01582aa3616bc9a6bd436e55a35c96d
yduclos <yduclos@sqli.com>
2025-11-18 16:08:09 +0100

    feat: :tada: Init architecture of the application

---
+++ src/main/java/com/biblio/App.java
@@ -0,0 +1,7 @@
+package main.java.com.biblio;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Biblio Application!");
+    }
+}
---
+++ src/main/java/com/biblio/service/LoanService.java
@@ -0,0 +1,5 @@
+package main.java.com.biblio.service;
+
+public class LoanService {
+    
+}
